c80d374d5a86f4045f44c29935a8df23df26105c
Joop Sint Jago <j.sintjago@xs4all.nl>
2013-05-11 21:02:10 +0200

    First commit

---
+++ Realty.php
@@ -0,0 +1,18 @@
+<?php
+
+class Realty {
+  public $title;
+  public $ysm_id;
+  public $t_prijs;
+  public $wijk;
+  public $t_bouwjaar;
+  public $t_slaapkamers;
+  public $t_badkamers;
+  public $t_kavel;
+  public $t_woonoppervl;
+  public $project;
+  public $zwembad;
+  public $zeezicht;
+  public $oplevering;
+  public $omschrijving;
+}
